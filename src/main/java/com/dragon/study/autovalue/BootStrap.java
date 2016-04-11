8eb91002251af0b0b5253e4196ffcde814e474be
longzhe <dragonlong1986@126.com>
2016-04-11 17:56:20 +0800

    init project

---
+++ src/main/java/com/dragon/study/autovalue/BootStrap.java
@@ -0,0 +1,11 @@
+package com.dragon.study.autovalue;
+
+/**
+ * Created by dragon on 16/4/11.
+ */
+public class BootStrap {
+
+  public static void main(String[] args) {
+    System.out.println("hello gradle");
+  }
+}
